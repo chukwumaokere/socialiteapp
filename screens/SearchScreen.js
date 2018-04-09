4958de411dc499eddb70b87a079bbb8c559e6ab9
chukwumaokere <chukwumaokere@yahoo.com>
2018-04-09 02:27:49 +0000

    adding proper search screen and basic function

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { SearchBar } from 'react-native-elements';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
     title: 'Search',
   };
-
+  searchMethod = (term) => {
+	console.log('searching for ' + term);
+  }
+  clearText = () => {
+	this.search.clear();
+  }
   render() {
     return (
       <ScrollView style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        {/* <ExpoLinksView /> */}
+	<SearchBar
+		lightTheme
+		onChangeText= {(searchTerm) => {this.searchMethod(searchTerm)} }
+		icon={{ type: 'font-awesome', name: 'search' }}
+		placeholder='Search For...'
+		clearIcon={{color: '#86939e', name: 'close' }}
+		cancelButtonTitle="Cancel"
+		ref={search => this.search = search}
+		onCancel={this.clearText}
+		 />
       </ScrollView>
     );
   }
@@ -21,7 +37,7 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
+    paddingTop: 0,
     backgroundColor: '#fff',
   },
 });
