fcd93bb45c706b1e272882b0fa0fa1d39c14d2bb
chukwumaokere <chukwumaokere@yahoo.com>
2018-04-13 08:07:36 +0000

    adding full create account function and erroring

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,7 +1,7 @@
 import { Notifications } from 'expo';
 import { StackNavigator } from 'react-navigation';
 import React from 'react';
-import { ScrollView, StyleSheet, SafeAreaView, Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback, View, Image, TextInput, TouchableOpacity, Text } from 'react-native';
+import { ScrollView, StyleSheet, Alert, SafeAreaView, Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback, View, Image, TextInput, TouchableOpacity, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import { SearchBar } from 'react-native-elements';
 import MainTabNavigator from '../navigation/MainTabNavigator';
@@ -11,17 +11,45 @@ const bg = require('../assets/images/splashicon.png');
 
 export default class CreateAccount extends React.Component {
   static navigationOptions = {title: 'Create a New Account'}
+  state = {
+        username: '',
+        password: '',
+        email: '',
+        firstname: '',
+        lastname: '',
+        phone: '',
+  }
+  setAccountDetails = () => {
+  }
   createAccount = () => {
 	const {navigate} = this.props.navigation;
 	//some creaty things
 	//if successful, then do this
-	navigate('Login');
-  }
-  state = {
-	username: '',
-	password: '',
-	email: '',
-	phone: '',
+	const {username} = this.state;
+	const {password} = this.state;
+	const {email} = this.state;
+	const {firstname} = this.state;
+	const {lastname} = this.state;
+	const {phone} = this.state;
+	
+	fetch('http://chukwumaokere.com/socialite/register.php', {
+		method: 'post',
+		header: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			username: username,
+			password: password,
+			email: email,
+			firstname: firstname,
+			lastname: lastname,
+			phone: phone
+		})
+	}).then( (response) => response.json() )
+                .then( (responseJson) => { Alert.alert(responseJson)} )
+                .catch( (error) => {console.error(error)} );
+	//navigate('Login');
   }
   clearText = () => {
 	this.search.clear();
@@ -40,10 +68,12 @@ export default class CreateAccount extends React.Component {
 			<Text style={styles.h2}>All your social media in one place</Text>
 		</View>
 		<View style={styles.infoContainer}>
-			<TextInput style={styles.input} returnKeyType='next' placeholder="Choose A Username..." placeholderTextColor="rgba(0,0,0,0.3)" onSubmitEditing={()=> this.refs.txtPassword.focus()} autoCaptialize={'none'} autoCorrect={false} />
-			<TextInput style={styles.input} returnKeyType='next'  placeholder="Enter A Password..." placeholderTextColor="rgba(0,0,0,0.3)" secureTextEntry={true} autoCorrect={false} ref={'txtPassword'} onSubmitEditing={()=> this.refs.email.focus()} autoCaptialize={'none'} autoCorrect={false} />
-			<TextInput style={styles.input} returnKeyType='next' placeholder="Enter Your Email Address..." placeholderTextColor="rgba(0,0,0,0.3)" ref={'email'} keyboardType={'email-address'} onSubmitEditing={()=> this.refs.phonen.focus()} autoCaptialize={'none'} autoCorrect={false} />
-			<TextInput style={styles.input} returnKeyType='go' keyboardType={'numeric'} placeholder="Enter Your Phone Number..." placeholderTextColor="rgba(0,0,0,0.3)" ref={'phonen'} autoCaptialize={'none'} autoCorrect={false} />
+			<TextInput style={styles.input} returnKeyType='next' placeholder="Enter your First Name..." placeholderTextColor="rgba(0,0,0,0.3)" onSubmitEditing={()=> this.refs.lastn.focus()} autoCaptialize={'none'} autoCorrect={false} onChangeText={(something) => {this.setState({firstname: something})}} />
+			<TextInput style={styles.input} returnKeyType='next' placeholder="Enter your Last Name..." placeholderTextColor="rgba(0,0,0,0.3)" ref={'lastn'} onSubmitEditing={()=> this.refs.usern.focus()} autoCaptialize={'none'} autoCorrect={false} onChangeText={(something) => {this.setState({lastname: something})}} />
+			<TextInput style={styles.input} returnKeyType='next' placeholder="Choose A Username..." placeholderTextColor="rgba(0,0,0,0.3)" ref={'usern'} onSubmitEditing={()=> this.refs.txtPassword.focus()} autoCaptialize={'none'} autoCorrect={false} onChangeText={(something) => {this.setState({username: something})}} />
+			<TextInput style={styles.input} returnKeyType='next'  placeholder="Enter A Password..." placeholderTextColor="rgba(0,0,0,0.3)" secureTextEntry={true} autoCorrect={false} ref={'txtPassword'} onSubmitEditing={()=> this.refs.email.focus()} autoCaptialize={'none'} autoCorrect={false} onChangeText={(something) => {this.setState({password: something})}} />
+			<TextInput style={styles.input} returnKeyType='next' placeholder="Enter Your Email Address..." placeholderTextColor="rgba(0,0,0,0.3)" ref={'email'} keyboardType={'email-address'} onSubmitEditing={()=> this.refs.phonen.focus()} autoCaptialize={'none'} autoCorrect={false} onChangeText={(something) => {this.setState({email: something})}} />
+			<TextInput style={styles.input} returnKeyType='go' keyboardType={'numeric'} placeholder="Enter Your Phone Number..." placeholderTextColor="rgba(0,0,0,0.3)" ref={'phonen'} autoCaptialize={'none'} autoCorrect={false} onChangeText={(something) => {this.setState({phone: something})}} />
 			<TouchableOpacity onPress={this.createAccount} >
 				<View style={styles.button}>
 					<Text style={{fontSize: 20, color: 'white',}}>CREATE ACCOUNT</Text>
